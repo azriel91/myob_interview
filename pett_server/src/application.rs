e2cd556e39f1c71eaa258bca3e102acbdaf90805
Azriel Hoh <azriel91@gmail.com>
2020-06-12 21:43:29 +1200

    Ignore coverage for application `root_dir` function.
    This is not possible to test, as the `CARGO_MANIFEST_DIR` environment
    variable is always set when running tests.

--- pett_server/src/application.rs
+++ pett_server/src/application.rs
@@ -8,25 +8,22 @@ impl Application {
     /// During development, this is the crate's directory, read from
     /// `CARGO_MANIFEST_DIR`. In release, this is the directory containing
     /// the executable.
+    #[cfg_attr(tarpaulin, skip)]
     pub(crate) fn root_dir() -> Result<PathBuf, io::Error> {
         if let Some(manifest_dir) = env::var_os("CARGO_MANIFEST_DIR") {
             return Ok(PathBuf::from(manifest_dir));
         }
 
-        // This section never be called without the environment variable.
-        #[cfg_attr(tarpaulin, skip)]
-        {
-            let mut exe_path = env::current_exe()?;
+        let mut exe_path = env::current_exe()?;
 
-            // Modify in-place to avoid an extra copy.
-            if exe_path.pop() {
-                return Ok(exe_path);
-            }
-
-            Err(io::Error::new(
-                io::ErrorKind::Other,
-                "Failed to find an application root",
-            ))
+        // Modify in-place to avoid an extra copy.
+        if exe_path.pop() {
+            return Ok(exe_path);
         }
+
+        Err(io::Error::new(
+            io::ErrorKind::Other,
+            "Failed to find an application root",
+        ))
     }
 }
